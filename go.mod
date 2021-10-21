5e36f164c3dd589526aec309ba35ecacbcb8155d
shen_gong <shen_gong@intsig.net>
2021-10-21 11:44:48 +0800

    init

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gongshen/xxl-job-executor
 
 go 1.16
 
-require github.com/feixiaobo/go-xxl-job-client/v2 v2.1.1
+require (
+	github.com/feixiaobo/go-xxl-job-client/v2 v2.1.1
+	github.com/jinzhu/configor v1.2.1
+)
--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"github.com/feixiaobo/go-xxl-job-client/v2"
 	"github.com/feixiaobo/go-xxl-job-client/v2/option"
+	"github.com/jinzhu/configor"
+	"log"
 )
 
+type Config struct {
+	AppName      string `yaml:"app_name"`
+	ClientPort   int    `yaml:"client_port"`
+	AdminAddress string `yaml:"admin_address"`
+}
+
 func main() {
+	cnf := &Config{}
+	if err := configor.Load(cnf, "config.yaml"); err != nil {
+		log.Fatalf("failed to load local config file:%v", err)
+	}
 	client := xxl.NewXxlClient(
-		option.WithAppName("test"),
-		option.WithClientPort(8088),
-		option.WithAdminAddress("http://172.16.243.253:8088/xxl-job-admin"),
+		option.WithAppName(cnf.AppName),
+		option.WithClientPort(cnf.ClientPort),
+		option.WithAdminAddress(cnf.AdminAddress),
 		option.WithEnableHttp(true),
 	)
 	client.Run()
